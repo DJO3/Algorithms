65464404fd6f1d89bcd8f66d5000203c0a232b1f
Dave O'Connor <dev@pytools.org>
2015-06-28 19:51:00 -0400

    added quickfind test

--- quickfind.py
+++ quickfind.py
@@ -5,11 +5,11 @@ class Quickfind_Eager:
 
     # Joins two nodes into a component
     def union(self, first_node, second_node):
-        print(self.array)
+        # print(self.array)
         for pos, val in enumerate(self.array):
             if self.array[pos] == self.array[first_node]:
                 self.array[pos] = self.array[second_node]
-        print(self.array)
+        # print(self.array)
         return '{0} is now joined to {1}'.format(first_node, second_node)
 
     # Checks if two nodes are in the same component
@@ -17,11 +17,15 @@ class Quickfind_Eager:
         return self.array[first_node] == self.array[second_node]
 
 
-v = Quickfind_Eager(5)
-print(v.union(1,2))
-print(v.connected(1,2))
-print(v.union(4,2))
-print(v.union(3,4))
-print(v.connected(0,2))
-print(v.connected(1,4))
-print(v.union(0,3))
+def test_quickfind(quickfind):
+    t = quickfind(100)
+    t.union(1,2)
+    print('1) OK!' if True == t.connected(1,2) else False)
+    t.union(4,2)
+    t.union(3,4)
+    print('2) OK!' if False == t.connected(0,2) else False)
+    print('3) OK!' if True == t.connected(1,4) else False)
+    t.union(0,3)
+    print('4) OK!' if True == t.connected(0,4) else False)
+
+test_quickfind(Quickfind_Eager)
